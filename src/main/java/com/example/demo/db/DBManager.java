c7cafbc98f3cec31cb04a8cd352df2817c219230
jiyoung park <miracle48693@naver.com>
2023-07-01 20:07:10 +0900

    initial commit

---
+++ src/main/java/com/example/demo/db/DBManager.java
@@ -0,0 +1,33 @@
+package com.example.demo.db;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.example.demo.vo.UserVO;
+
+public class DBManager {
+	
+	public static SqlSessionFactory sqlSessionFactory;
+	static {
+		try {
+			String resource = "com/example/demo/db/sqlMapConfig.xml";
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory =
+			  new SqlSessionFactoryBuilder().build(inputStream);
+		}catch (Exception e) {
+			System.out.println("예외발생:"+e.getMessage());
+		}
+	}
+	
+	public static List<UserVO> findAll() {
+		SqlSession session = sqlSessionFactory.openSession();
+		List<UserVO> list = session.selectList("user.findAll");
+		session.close();
+		return list;
+	}
+}
