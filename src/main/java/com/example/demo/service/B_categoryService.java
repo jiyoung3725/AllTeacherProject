69be4b8fe474d89f5e1907f7a05a69e7c702cafa
jiyoung park <miracle48693@naver.com>
2023-07-12 22:19:28 +0900

    categoryService작성

--- src/main/java/com/example/demo/service/B_categoryService.java
+++ src/main/java/com/example/demo/service/B_categoryService.java
@@ -1,9 +1,12 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.B_categoryDAO;
+import com.example.demo.vo.B_categoryVO;
 
 import lombok.Setter;
 
@@ -13,4 +16,19 @@ public class B_categoryService {
 
 	@Autowired
 	private B_categoryDAO dao;
+	
+	public List<B_categoryVO> findAll(){
+		return dao.findAll();
+	}
+	
+	public B_categoryVO save(B_categoryVO c) {
+		return dao.save(c);
+	}
+	public B_categoryVO getOne(int categoryNo) {
+		return dao.getOne(categoryNo);
+	}
+	
+	public void deleteById(int categoryNo) {
+		dao.deleteById(categoryNo);
+	}
 }
